05e9e702ff2d6c3995bbcff4b1cd8c70b6f48e47
alvaro <alvher.orellana@gmail.com>
2021-02-08 19:51:43 -0300

    innital commit

---
+++ Rentify/Models/Pelicula.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace Rentify.Models
+{
+    public class Pelicula
+    {
+        public Pelicula()
+        {
+        }
+
+
+        public int Id { get; set; }
+        public String Nombre { get; set; }
+    }
+}
