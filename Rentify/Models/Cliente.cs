a43446aadb878efdf7af006062f38af8fd92f794
alvaro <alvher.orellana@gmail.com>
2021-02-09 01:16:02 -0300

    Extraccion de navbar en una partialView

---
+++ Rentify/Models/Cliente.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Rentify.Models
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+        }
+
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ Rentify/ViewModels/PeliculaRandomViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using Rentify.Models;
+
+namespace Rentify.ViewModels
+{
+    public class PeliculaRandomViewModel
+    {
+        public PeliculaRandomViewModel()
+        {
+        }
+
+        public Pelicula Pelicula { get; set; }
+        public List<Cliente> Clientes { get; set; }
+    }
+}
