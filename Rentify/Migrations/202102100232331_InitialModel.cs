81395b1ebdeaebb0c7f44c7862a79f67a1065cca
alvaro <alvher.orellana@gmail.com>
2021-02-09 23:42:08 -0300

    Inclusión de EntityFramwork
    Agregue nuevas dependencias para hacer que funcionase EF. Puede que haga falta agregar una dependencia as de asp.net.identity

---
+++ Rentify/Migrations/202102100232331_InitialModel.cs
@@ -0,0 +1,36 @@
+﻿namespace Rentify.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitialModel : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Clientes",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nombre = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            CreateTable(
+                "dbo.Peliculas",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Nombre = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Peliculas");
+            DropTable("dbo.Clientes");
+        }
+    }
+}
---
+++ Rentify/Models/MyDBContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Data.Entity;
+
+namespace Rentify.Models
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext()
+        {
+
+        }
+        public DbSet<Cliente> Clientes { get; set; } // My domain models
+        public DbSet<Pelicula> Peliculas { get; set; }// My domain models
+    }
+}
+
+
+
