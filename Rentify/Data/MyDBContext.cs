f33137da85ed018102793c63da02a8f81760122a
alvaro <alvher.orellana@gmail.com>
2021-02-10 22:51:00 -0300

    added code from EF documentation Modified the code according to the microsoft docs to make EF work. Still cant make it work tho

---
+++ Rentify/Data/MyDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Rentify.Models;
+
+namespace Rentify.Data
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext(DbContextOptions<MyDBContext> options)
+                   : base(options)
+        {
+        }
+
+
+        public DbSet<Cliente> Clientes { get; set; } 
+        public DbSet<Pelicula> Peliculas { get; set; }
+    }
+}
+
+
+
